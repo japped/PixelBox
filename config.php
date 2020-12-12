2b346d9ffba9e360607daa6c02eb14e5e38c175f
japped <64382915+japped@users.noreply.github.com>
2020-12-13 01:25:16 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 // mysql konfigurering
 mysql_connect(
 
-$host = "sql2.freesqldatabase.com";
-$database = "sql2381755";
-$username = "sql2381755";
-$password = "eX7*vW5*";
+$host = "sql2.freesqldatabase.com",
+$database = "sql2381755",
+$username = "sql2381755",
+$password = "eX7*vW5*",
 
 
 // annen konfigurering
