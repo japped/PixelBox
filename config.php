f1c7f20a4f53f90dc91e0cab153bd00f66fba9d9
japped <64382915+japped@users.noreply.github.com>
2020-12-13 01:30:46 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 // mysql konfigurering
 
-$host = "sql2.freesqldatabase.com",
-$database = "sql2381755",
-$username = "sql2381755",
-$password = "eX7*vW5*",
+$host = "sql2.freesqldatabase.com"
+$database = "sql2381755"
+$username = "sql2381755"
+$password = "eX7*vW5*"
 
 
 // annen konfigurering
-$theme = "wuax_2_1",
-$redirect = "http://www.google.com",
+$theme = "wuax_2_1"
+$redirect = "http://www.google.com"
 
-error_reporting(E_ALL ^ E_DEPRECATED),
+error_reporting(E_ALL ^ E_DEPRECATED)
 
 ?>
