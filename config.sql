cc16cf7dfc56cca64d965df7560542046e66b2e4
japped <64382915+japped@users.noreply.github.com>
2020-12-13 01:37:29 +0200

    Create config.sql

---
+++ config.sql
@@ -0,0 +1,19 @@
+mysql_connect(
+
+$host = "sql2.freesqldatabase.com"
+
+$database = "sql2381755"
+
+$username = "sql2381755"
+
+$password = "eX7*vW5*"
+
+
+
+
+$theme = "wuax_2_1"
+
+$redirect = "http://www.google.com"
+
+error_reporting(E_ALL ^ E_DEPRECATED)
+)
