5988d626234395c157a24e95744ff2fa1cf7fe11
japped <64382915+japped@users.noreply.github.com>
2020-12-13 01:39:04 +0200

    Update config.sql

--- config.sql
+++ config.sql
@@ -1,5 +1,4 @@
 mysql_connect;
-
 $host = "sql2.freesqldatabase.com";
 $database = "sql2381755";
 $username = "sql2381755";
@@ -7,4 +6,3 @@ $password = "eX7*vW5*";
 $theme = "wuax_2_1";
 $redirect = "http://www.google.com";
 error_reporting(E_ALL ^ E_DEPRECATED);
-)
